0414de460a142c7db98cd04e4101057a8dbe0abe
Matias Rottbøll <mrottboell@gmail.com>
2024-11-07 12:41:42 +0100

    US-7
    sets up dtos, facade and Api so that us-7 could be made

---
+++ RestaurantService/RestaurantService/DTOs/AddressDTO.cs
@@ -0,0 +1,34 @@
+﻿namespace RestaurantService.DTOs;
+
+public class AddressDTO
+{
+    public int Id { get; set; } //primary key
+    public String Street { get; set; }
+    public String City { get; set; }
+    public String ZipCode{ get; set; }
+    public String Region { get; set; }
+    
+    
+    public AddressDTO(int id, String street, String city, String zipCode, String region)
+    {
+        Id = id;
+        Street = street;
+        City = city;
+        ZipCode = zipCode;
+        Region = region;
+    }
+    //empty
+    public AddressDTO()
+    {
+        
+    }
+    
+    //for creating a new address
+    public AddressDTO(String street, String city, String zipCode, String region)
+    {
+        Street = street;
+        City = city;
+        ZipCode = zipCode;
+        Region = region;
+    }
+}
---
+++ RestaurantService/RestaurantService/DTOs/MenuItemDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace RestaurantService.DTOs;
+
+public class MenuItemDTO
+{
+    public int Id { get; set; } //primary key
+    public String ItemName { get; set; }
+    public double Price  { get; set; }
+    public String ItemDescription { get; set; }
+    
+    public MenuItemDTO(int id, String itemName, double price, String itemDescription)
+    {
+        Id = id;
+        ItemName = itemName;
+        Price = price;
+        ItemDescription = itemDescription;
+    }
+    
+    public MenuItemDTO()
+    {
+        
+    }
+    
+    //for creating a new menu item
+    public MenuItemDTO(String itemName, double price, String itemDescription)
+    {
+        ItemName = itemName;
+        Price = price;
+        ItemDescription = itemDescription;
+    }
+
+}
