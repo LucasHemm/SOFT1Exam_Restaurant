0622b91a5e24bb6b52e4c79f38dcb30654d619dd
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2024-11-07 09:11:01 +0100

    Remakes the structure of the project, and adds workflow file

---
+++ RestaurantService/RestaurantServiceTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace RestaurantServiceTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        int one = 1;
+        int two = 2;
+        Assert.Equal(3, one + two);
+    }
+}
