5b062c952cb35ba04aa7bba7ae32f27553b470ff
Matias Rottbøll <mrottboell@gmail.com>
2024-11-07 10:54:43 +0100

    makes test work again

--- RestaurantService/RestaurantServiceTest/UnitTest1.cs
+++ RestaurantService/RestaurantServiceTest/UnitTest1.cs
@@ -5,7 +5,7 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        int one = 1;
+        int one = 2;
         int two = 2;
         Assert.Equal(4, one + two); //test
     }
